5ca2d2f9ec8f5cb51ddcf826b153f1269bdf97f9
Franklin-design77 <franklinphilip60@gmail.com>
2026-08-30 20:50:54 +0530

    Enhance StartEnd.java with problem statement
    Added a detailed problem statement and input/output format for the Activity Selection Problem.

--- StartEnd.java
+++ StartEnd.java
@@ -1,3 +1,33 @@
+/*Activity Selection Problem – Question
+Problem Statement
+
+You are given N activities, where each activity has a start time and an end time.
+
+You can select an activity only if its start time is greater than or equal to the end time of the previously selected activity.
+
+Your task is to find the maximum number of non-overlapping activities that can be selected.
+
+To solve the problem, use the Greedy Algorithm by sorting the activities according to their end time.
+
+Input Format
+The first line contains an integer N, representing the number of activities.
+The next N lines contain two integers:
+start – starting time of the activity
+end – ending time of the activity
+Output Format
+
+Print the maximum number of activities that can be selected without overlapping.
+
+Sample Input
+5
+1 3
+2 4
+3 5
+5 7
+6 8
+Sample Output
+3*/
+
 import java.util.*;
 import java.util.Scanner;
 public class StartEnd{
